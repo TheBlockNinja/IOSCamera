f6c72173f21083e2429c0886e9c14ea45e49efc0
Seann Moser <seannsea@gmail.com>
2018-10-31 10:58:20 -0700

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by Seann Moser on 10/31/18.
+//  Copyright © 2018 SOU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
